fce29c32870130adb8a93399e694a4a693e5bad7
Andrea N <andrea@narbot.com>
2023-01-11 16:20:25 -0500

    Update Alert.js

---
+++ components/alert.js
@@ -0,0 +1,29 @@
+// import Container from './container'
+// import cn from 'classnames'
+
+// export default function Alert() {
+//   return (
+//     <div
+//       className={cn('border-b',
+//         'bg-accent-1 border-accent-2')}
+//     >
+//       <Container>
+//         <div className="py-2 text-center text-sm">
+//           <>
+//             The source code for this template is available on the Solutions Engineering{' '}
+//             <a
+//               href={`https://github.com/Netlify-Moneytronic`}
+//               className="underline hover:text-success duration-200 transition-colors"
+//             >
+//               GitHub
+//             </a>
+//             .
+//           </>
+//         </div>
+//       </Container>
+//     </div>
+//   )
+// }
+//I want to remove all the code from the Alert component and not replace it 
+
+//
